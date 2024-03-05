ca2b788f1123d89e9a4165df7216aa8035db5b66
ArinKUNUR1 <hardlife3773@gmail.com>
2024-03-05 20:34:10 +0300

    first commit

---
+++ ben-10/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <img src="./images/gread.png"/>
+    </div>
+  );
+}
+
+export default App;
